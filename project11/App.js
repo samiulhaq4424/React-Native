e6e5db956323488a5237665719ab7d28fd53d4f0
samiulhaq4424 <samiulhaq54424@gmail.com>
2024-08-26 00:48:50 +0530

    second-commit

--- project11/App.js
+++ project11/App.js
@@ -1,29 +1,71 @@
 /* eslint-disable */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet,Text, TouchableHighlight, View} from 'react-native';
 
+/**
+ * 1) Style with Button (Note: default <Button> bhot jyada styles ko support nahi karta), so we have alternatives i.e. TouchableHighlight & TouchableOpacity (component: TouchHighLightComp)
+ * 2) Radio Button (in React-Native: radio-button nai hota using import)
+*/
 
-
+import TouchHighLightComp from './components/TouchHighLightComp';
 
 const App = () => {
   
   return (
-    <View>
-      <Text>Hiiii</Text>
+    <View style={styles.main}>
+      
+      {/* <TouchHighLightComp/> */}
+
+      {/* for radio button use TouchableOpacity as isme kaam less krna hoga and not TouchableHighlight as isme jyada kaam karna parega */}
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+
+
+
+
+
+
+
+
+
+
+  main:{
+    flex: 1,
+  },
+  button:{
+    backgroundColor: 'grey',
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'center',
+    padding: 10,
+    margin: 10,
+    borderRadius: 30,
+    //for shadow around the button, we use elevation in android, and for shadow color use shadowColor
+    shadowColor: 'red',
+    elevation: 15, // Shadow for Android
+    shadowOpacity: 0.8, //defines how transparent or opaque the shadow will be, with values ranging from 0 (completely transparent) to 1 (completely opaque).
+  },
+
+  success:{
+    backgroundColor: 'green',
+  },
+
+  primary:{
+    backgroundColor: 'blue',
+  },
+
+  warning:{
+    backgroundColor: 'gold',
+  },
+
+  error:{
+    backgroundColor: 'red',
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
---
+++ project11/components/TouchHighLightComp.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+
+import React from 'react';
+
+import {StyleSheet,Text, TouchableHighlight, View} from 'react-native';
+
+const TouchHighLightComp = () => {
+  
+  return (
+    <View style={styles.main}>
+      <Text style={{fontSize:30, color: 'green', textAlign: 'center'}}>TouchableHighlight</Text>
+
+      {/* apply style to Text for button and not to TouchableHighlight */}
+      <TouchableHighlight> 
+        <Text style={[styles.button, styles.success]}>Success</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight> 
+        <Text style={[styles.button, styles.primary]}>Primary</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight> 
+        <Text style={[styles.button, styles.warning]}>Warning</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight> 
+        <Text style={[styles.button, styles.error]}>Error</Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main:{
+    flex: 1,
+  },
+
+  button:{
+    backgroundColor: 'grey',
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'center',
+    padding: 10,
+    margin: 10,
+    borderRadius: 30,
+    //for shadow around the button, we use elevation in android, and for shadow color use shadowColor
+    shadowColor: 'red',
+    elevation: 15, // Shadow for Android
+    shadowOpacity: 0.8, //defines how transparent or opaque the shadow will be, with values ranging from 0 (completely transparent) to 1 (completely opaque).
+  },
+
+  success:{
+    backgroundColor: 'green',
+  },
+
+  primary:{
+    backgroundColor: 'blue',
+  },
+
+  warning:{
+    backgroundColor: 'gold',
+  },
+
+  error:{
+    backgroundColor: 'red',
+  },
+});
+
+export default TouchHighLightComp;

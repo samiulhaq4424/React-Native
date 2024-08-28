ee29098409601f7436cc8571238edf986eadce5b
samiulhaq4424 <samiulhaq54424@gmail.com>
2024-08-28 23:19:27 +0530

    second-commit

--- project15/App.js
+++ project15/App.js
@@ -1,45 +1,23 @@
 /* eslint-disable */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
 
+/**
+ * 1) Simple Api Call (component: ApiCall)
+*/
+
+import ApiCall from './Components/ApiCall';
 
 
 const App = () => {
   
   return (
-    <View>
-      <Text>Hiiii</Text>
-    </View>
+    
+    <ApiCall/>
+
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ project15/Components/ApiCall.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+
+
+// https://jsonplaceholder.typicode.com/
+
+import { useEffect, useState } from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+const ApiCall = () => {
+  
+  const [data, setData] = useState('');
+
+  const getAPIdata = async () =>{ //api call
+    
+    const url ='https://jsonplaceholder.typicode.com/posts/1'; 
+    const result = await fetch(url); // Fetch the data from the API; say api me data bhot jyada ha say 600Kb or 1Mb, usme load hone me time lag sakta ha, so woh humare application ko slow na ho, wo isse aage jane deta ha aur jab ye api call hogi tab isse display kardega so we use 'await' so also using 'async' function 
+
+    const dataObjFetched = await result.json(); // Parse the result as JSON i.e  resolving to a JavaScript object (or array) representing the JSON data.
+
+    // console.warn(dataObjFetched);
+    setData(dataObjFetched)
+
+
+  }
+
+  // Using useEffect to call getAPIdata when the component mounts
+  useEffect(()=>{
+    /*
+        getAPIdata() function is called inside the useEffect hook to fetch data from the API when the component mounts. 
+        Without useEffect, if you call getAPIdata directly in the component, it would get called on every render, which is inefficient and usually undesirable. useEffect ensures that the API call is made only once when the component first loads.
+    */
+    getAPIdata();
+  },[]);
+
+  return (
+    <View>
+      <Text style={{fontSize: 30, textAlign: 'center'}} >API Call</Text>
+      {
+         data ? 
+         <View>
+           <Text style={{fontSize: 22, marginBottom: 10}}>userId: {data.userId}</Text>
+           <Text style={{fontSize: 22, marginBottom: 10}}>Id: {data.id}</Text>
+           <Text style={{fontSize: 22, marginBottom: 10}}>Title: {data.title}</Text>
+           <Text style={{fontSize: 22, marginBottom: 10}}>Body: {data.body}</Text>
+         </View> : ''
+      }
+    </View>
+  );
+};
+
+export default ApiCall;
